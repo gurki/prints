1c935634bd4c1b6d2c4bf132b69b33fa6b4fcbb5
Tobias Gurdan <tobias@gurdan.de>
2024-12-14 13:11:21 +0100

    Unify single and double with open and closed modes

--- diffusor/tube.js
+++ diffusor/tube.js
@@ -2,25 +2,44 @@ const { draw, drawRectangle, drawCircle } = replicad;
 
 const defaultParams = {
   stripWidth: 11,
-  stripHeight: 2.8,
-  tubeThickness: 0.45,
   tubeDiameter: 25,
-  innerThickness: 0.45,
-  tubeHeight: 64
+  tubeHeight: 64,
+  mode: "l2c",
 };
 
+const offsets = {
+  "l1o": 0.92,
+  "l1c": -0.86,
+  "l2o": 1.83,
+  "l2c": 0.12,
+}
 
 const main = ( _, params ) => {
 
+  const open = [ "l1o", "l2o" ].includes( params.mode );
+  const single = [ "l1o", "l1c" ].includes( params.mode );
+
+  const height = open ? 1.2 : 2.8;
+  const tubeThickness = single ? 0.45 : 0.9;
+  const canalThickness = tubeThickness;
+  const offset = offsets[ params.mode ];
+
   const d = params.tubeDiameter;
   const r1 = params.tubeDiameter / 2;
-  const r2 = r1 - params.tubeThickness;
+  const r2 = r1 - tubeThickness;
 
-  const innerCanal = drawRectangle( params.stripWidth, params.stripHeight, params.stripHeight / 4 )
-  const canal = innerCanal
-    .offset( params.innerThickness )
+  const innerCanal = drawRectangle( params.stripWidth, height, height / 4 )
+  let canal = innerCanal
+    .offset( canalThickness )
     .cut( innerCanal )
-    .translate( 0, - params.stripHeight / 2 - params.innerThickness );
+    .translate( 0, - height / 2 - canalThickness );
+  
+  if ( open ) {
+    canal = canal.cut( 
+      drawRectangle( params.stripWidth - 2 * 2, canalThickness )
+      .translate( 0, -canalThickness / 2 ) 
+    );
+  }
 
   let tube = drawCircle( r1 ).cut( drawCircle( r2 ) );
   tube = tube.cut( 
@@ -28,8 +47,8 @@ const main = ( _, params ) => {
     .translate( 0, - r1  ) 
   )
   
-  const drawing = tube.fuse( canal.translate( 0, - r1 + 2 * params.stripHeight - 0.86 ) );
-  // return drawing;
+  const drawing = tube.fuse( canal.translate( 0, - r1 + 2 * height + offset ) );
+  // return drawing
   return drawing.sketchOnPlane().extrude( params.tubeHeight );
   
 };
