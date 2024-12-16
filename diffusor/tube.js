b138ef822fb9e84f202586e361d48259c9949490
Tobias Gurdan <tobias@gurdan.de>
2024-12-16 19:12:46 +0100

    Add 20mm variant

--- diffusor/tube.js
+++ diffusor/tube.js
@@ -2,17 +2,28 @@ const { draw, drawRectangle, drawCircle } = replicad;
 
 const defaultParams = {
   stripWidth: 11,
-  tubeDiameter: 25,
+  tubeDiameter: 20,
   tubeHeight: 64,
   mode: "l2c",
 };
 
-const offsets = {
+const offsets25 = {
   "l1o": 0.92,
   "l1c": -0.86,
-  "l2o": 1.83,
-  "l2c": 0.12,
+  "l2o": 1.71,
+  "l2c": -0.01,
 }
+
+const offsets20 = {
+  "l1o": 1.31,
+  "l1c": -0.5,
+  "l2o": 2.17,
+  "l2c": 0.39,
+}
+
+const offsets = offsets20;
+const factor = 0.5; //  0.0;
+
 
 const main = ( _, params ) => {
 
@@ -20,7 +31,7 @@ const main = ( _, params ) => {
   const single = [ "l1o", "l1c" ].includes( params.mode );
 
   const height = open ? 1.2 : 2.8;
-  const tubeThickness = single ? 0.45 : 0.9;
+  const tubeThickness = single ? 0.45 : 0.84;
   const canalThickness = tubeThickness;
   const offset = offsets[ params.mode ];
 
@@ -43,12 +54,44 @@ const main = ( _, params ) => {
 
   let tube = drawCircle( r1 ).cut( drawCircle( r2 ) );
   tube = tube.cut( 
-    drawRectangle( params.stripWidth, d )
+    drawRectangle( params.stripWidth + canalThickness * factor, d )
     .translate( 0, - r1  ) 
   )
   
   const drawing = tube.fuse( canal.translate( 0, - r1 + 2 * height + offset ) );
-  // return drawing
+  return drawing
+
+  //  plate
+  // const cx = 4;
+  // const cy = 5;
+  // const m = 2;
+  // const h = 6;
+
+  // let standPlate = drawRectangle( ( 2 * r1 + m ) * cx + m, ( 2 * r1 + m ) * cy + m, m )
+  //   .sketchOnPlane()
+  //   .extrude( -1 );
+
+  // let stand = drawCircle( r2 - 0.2 )
+  //   .cut( drawRectangle( 2 * r2, r2 ).translate( 0, -r2 ))
+  // const x0 = - ( ( 2 * r1 + m ) * ( cx - 1 ) ) / 2;
+  // const y0 = - ( ( 2 * r1 + m ) * ( cy - 1 ) ) / 2;
+  
+  // for ( let x = 0; x < cx; x++ ) {
+  //   for ( let y = 0; y < cy; y++ ) {
+  //     let curr = stand
+  //       .clone()
+  //       .translate( x0 + ( 2 * r1 + m ) * x, y0 + ( 2 * r1 + m ) * y )
+  //       .sketchOnPlane()
+  //       .extrude( h );
+  //     standPlate = standPlate.fuse( curr );
+  //   }
+  // }
+
+  // return standPlate.fillet( 2, e => e.and( [ 
+  //   e => e.ofCurveType( "CIRCLE" ),   
+  //   e => e.inPlane( "XY", h )
+  // ]));
+
   return drawing.sketchOnPlane().extrude( params.tubeHeight );
   
 };
